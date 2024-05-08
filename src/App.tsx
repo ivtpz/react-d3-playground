2f7ed42f172f91bbddb1ae3c630fa117fd0bf7f7
adamtopaz <github@adamtopaz.com>
2024-05-07 21:28:52 -0600

    add graph

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,41 @@ import { useEffect, useRef } from "react";
 import "./App.css";
 import * as d3 from "d3";
 
+var nodes = [
+  {
+    "id" : 0,
+    "name" : "A"
+  },
+  {
+    "id" : 1,
+    "name" : "B"
+  },
+  {
+    "id" : 2,
+    "name" : "C"
+  },
+  {
+    "id" : 3,
+    "name" : "D"
+  },
+  {
+    "id" : 4,
+    "name" : "E"
+  },
+];
+
+var edges = [
+  [1,2],
+  [1,3],
+  [2,4],
+  [3,4]
+];
+
 function App() {
   const ref = useRef<HTMLDivElement>(null);
   useEffect(() => {
     if (ref.current) {
+      console.log(ref.current);
       d3.select(ref.current).selectAll("svg").remove();
       d3.select(ref.current)
         .append("svg")
