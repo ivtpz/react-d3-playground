d78dec4f8be6a550877958e738f44a876f505838
Ivey Topaz <itopaz@paperlesspost.com>
2024-05-07 21:48:36 -0600

    Add a force graph

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,39 @@
+// @ts-nocheck
 import { useEffect, useRef } from "react";
 import "./App.css";
-import * as d3 from "d3";
+import ForceGraph from "./ForceGraph";
 
 function App() {
   const ref = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
     if (ref.current) {
-      d3.select(ref.current).selectAll("svg").remove();
-      d3.select(ref.current)
-        .append("svg")
-        .attr("width", 800)
-        .attr("height", 600)
-        .append("circle")
-        .attr("cx", 150)
-        .attr("cy", 70)
-        .attr("r", 50)
-        .style("fill", "red");
+      const graph = ForceGraph(
+        {
+          nodes: [
+            { id: "Alice", group: 2 },
+            { id: "Bob", group: 1 },
+            { id: "Carol", group: 1 },
+            { id: "David", group: 2 },
+          ],
+          links: [
+            { source: "Bob", target: "Alice", value: 1 },
+            { source: "Carol", target: "Bob", value: 2 },
+            { source: "David", target: "Alice", value: 3 },
+          ],
+        },
+        {
+          nodeId: (d) => d.id,
+          nodeGroup: (d) => d.group,
+          nodeTitle: (d) => `${d.id}\n${d.group}`,
+          linkStrokeWidth: (l) => Math.sqrt(l.value),
+          width: 600,
+          height: 600,
+          nodeRadius: 10,
+        }
+      );
+      ref.current.innerHTML = "";
+      ref.current.appendChild(graph);
     }
   }, []);
   return (
